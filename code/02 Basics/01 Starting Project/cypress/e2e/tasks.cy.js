8b2384b559e7d7db78611b87921d319d3607ee13
shrinivassab <shrinivassab@gmail.com>
2023-05-20 22:13:40 -0400

    user interaction simulation with button click

--- code/02 Basics/01 Starting Project/cypress/e2e/tasks.cy.js
+++ code/02 Basics/01 Starting Project/cypress/e2e/tasks.cy.js
@@ -3,5 +3,12 @@ describe('tasks managements', () => {
         cy.visit('http://localhost:5173/')
         cy.contains('Add Task').click()
         cy.get('.backdrop').click({force: true})
+        cy.get('.backdrop').should('not.exist')
+        cy.get('.modal').should('not.exist')
+        // With cancel button
+        cy.contains('Add Task').click()
+        cy.contains('Cancel').click()
+        cy.get('.backdrop').should('not.exist')
+        cy.get('.modal').should('not.exist')
     });
 })
